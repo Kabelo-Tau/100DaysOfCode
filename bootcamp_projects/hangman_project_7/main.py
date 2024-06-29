c5cd950730b63ca9cac20580db251f83a9683657
Kabelo Tau <146898376+Kabelo-Tau@users.noreply.github.com>
2024-06-29 21:31:14 +0000

    Fixed spelling erros

--- bootcamp_projects/hangman_project_7/main.py
+++ bootcamp_projects/hangman_project_7/main.py
@@ -20,7 +20,7 @@ for _ in range(word_length):
 while not end_of_game:
     guess = input("\nGuess a letter: ").lower()
 
-    clear() # Clears consoloe when user guesses letter
+    clear() # Clears console when user guesses letter
     
     if guess in display:
         print(f"You've already guessed the letter {guess}.")
